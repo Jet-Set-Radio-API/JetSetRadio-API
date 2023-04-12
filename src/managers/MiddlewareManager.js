f52e42733e175d5f12f740ff6d2f2bef9865b680
Kaipo <kaipojames12@gmail.com>
2023-04-12 18:53:16 -0500

    Add endpoints route

--- src/managers/MiddlewareManager.js
+++ src/managers/MiddlewareManager.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import listEndpoints from 'express-list-endpoints';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import cors from 'cors';
@@ -28,10 +29,17 @@ class MiddlewareManager {
 
     app.get('/', (req, res) => renderHome(req, res));
     app.get('/health', (req, res) => res.send(healthCheckManager.getAppHealth()));
+    app.get('/endpoints', (req, res) => res.send(filterPipeRoutes(req, listEndpoints(app))));
     app.use('/v1/api', router);
     app.use('/docs', swaggerUi.serve, swaggerUi.setup(data))
   }
 
+}
+
+const filterPipeRoutes = (req, endpoints) => {
+  return endpoints
+    .filter(endpoint => req?.query?.pipe ? !endpoint.path.includes(':') && endpoint.path.includes('/v1/api') : endpoint)
+    .map(endpoint => { return endpoint.path })
 }
 
 export default MiddlewareManager;
