893f4a13b28b33e30cfc80a0fd2e0989a9d3873a
Kaipo <kaipojames12@gmail.com>
2023-04-16 16:56:05 -0500

    Add Character endpoints

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import games from './gameRouter.js';
 import graffitiTags from './graffitiTagRouter.js';
 import songs from './songRouter.js';
 import artists from './artistRouter.js';
+import characters from './characterRouter.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,8 +12,9 @@ dotenv.config();
 const router = express.Router();
 
 router.use('/games', games);
-router.use('/graffiti-tags', graffitiTags);
 router.use('/songs', songs);
 router.use('/artists', artists);
+router.use('/graffiti-tags', graffitiTags);
+router.use('/characters', characters);
 
 export default router;
