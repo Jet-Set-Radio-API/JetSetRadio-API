f869c63ba6df57f17391af9d8aaa94cab203b38a
Kaipo Wilmeth <kwilmeth@fanthreesixty.com>
2025-09-03 20:33:48 -0500

    add docs

---
+++ src/public/js/examples/collectibleExample.js
@@ -0,0 +1,25 @@
+const collectibleResource = {
+  _id: "68b8d438abb00c8ec1347099",
+  gallery: [
+    {
+      name: "collectible",
+      image:
+        "https://static.wikia.nocookie.net/bomb-rush-cyberfunk/images/6/68/PyramidIslandMusic4.jpg/revision/latest/scale-to-width-down/1000?cb=20240809021710",
+    },
+  ],
+  name: "AGUA",
+  key: "AGUA-CD",
+  description:
+    "on a ledge near the gate that Rise guards in the beginning of the area. Face the gate after getting into the area from the ferry, it is on the ledge to the left of it. In order to get this CD, you need to get past said gate, then navigate until you're one level above the main floor. Then you need to drop down to the ledge from there.",
+  type: "CD",
+  unlockedByDefault: false,
+  gameId: "68b8d3cff2538137a7c2fc2e",
+  location: {
+    name: "Pyramid Island",
+    id: "68b8d41babb00c8ec1346e8f",
+  },
+  createdAt: "2025-09-03T23:50:16.591Z",
+  updatedAt: "2025-09-03T23:50:16.591Z",
+};
+
+export default collectibleResource;
