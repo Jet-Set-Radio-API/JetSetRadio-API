bf6904f575485cd834a74eb397770532d36831bb
Kaipo Wilmeth <kwilmeth@fanthreesixty.com>
2025-09-03 20:38:10 -0500

    fix

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -1,17 +1,14 @@
-import {
-  performJSRAction,
-  performJSRFAction,
-  performBRCAction,
-} from "../config/db.js";
-import {Actions} from "../config/dbActions.js";
-import {sortObjects} from "../utils/utility.js";
+import { performJSRAction, performJSRFAction, performBRCAction } from "../config/db.js";
+import { Actions } from "../config/dbActions.js";
+import { sortObjects } from "../utils/utility.js";
 
-const Song = "Song";
+
+const Song = 'Song';
 
 export const getSongs = async (req, res) => {
   try {
     const sortByValue = req?.query?.sortBy ? req?.query?.sortBy : undefined;
-    const sortOrder = req?.query?.orderBy ? req?.query?.orderBy : "asc";
+    const sortOrder = req?.query?.orderBy ? req?.query?.orderBy : 'asc';
     const jsrSongs = await fetchJSRSongs(req);
     const jsrfSongs = await fetchJSRFSongs(req);
     const brcSongs = await fetchBRCSongs(req);
@@ -20,10 +17,10 @@ export const getSongs = async (req, res) => {
       return res.send(songs.sort(sortObjects(sortByValue, sortOrder)));
     }
     res.send([...jsrSongs, ...jsrfSongs, ...brcSongs]);
-  } catch (err) {
+  } catch(err) {
     res.status(500).send(`Could not fetch ALL SONGS due to error: \n${err}`);
   }
-};
+}
 
 export const getJSRSongs = async (req, res) => {
   try {
@@ -32,10 +29,10 @@ export const getJSRSongs = async (req, res) => {
       return res.send(jsrSongs);
     }
     res.status(404).send();
-  } catch (err) {
+  } catch(err) {
     res.status(500).send(`Could not fetch JSR SONGS \n${err}`);
   }
-};
+}
 
 export const getJSRSongById = async (req, res) => {
   try {
@@ -43,15 +40,11 @@ export const getJSRSongById = async (req, res) => {
     if (jsrSong) {
       return res.send(jsrSong);
     }
-    res
-      .status(404)
-      .send(`JSR Song Resource could not be found at requested location`);
-  } catch (err) {
-    res
-      .status(500)
-      .send(`Could not fetch JSR SONG with ID: ${req.params.id} \n${err}`);
+    res.status(404).send(`JSR Song Resource could not be found at requested location`);
+  } catch(err) {
+    res.status(500).send(`Could not fetch JSR SONG with ID: ${req.params.id} \n${err}`);
   }
-};
+}
 
 export const getJSRFSongs = async (req, res) => {
   try {
@@ -60,10 +53,10 @@ export const getJSRFSongs = async (req, res) => {
       return res.send(jsrfSongs);
     }
     res.status(404).send();
-  } catch (err) {
+  } catch(err) {
     res.status(500).send(`Could not fetch JSRF Songs \n${err}`);
   }
-};
+}
 
 export const getJSRFSongById = async (req, res) => {
   try {
@@ -71,15 +64,11 @@ export const getJSRFSongById = async (req, res) => {
     if (jsrfSong) {
       return res.send(jsrfSong);
     }
-    res
-      .status(404)
-      .send(`JSRF Song Resource could not be found at requested location`);
-  } catch (err) {
-    res
-      .status(500)
-      .send(`Could not fetch JSRF SONG with ID: ${req.params.id} \n${err}`);
+    res.status(404).send(`JSRF Song Resource could not be found at requested location`);
+  } catch(err) {
+    res.status(500).send(`Could not fetch JSRF SONG with ID: ${req.params.id} \n${err}`);
   }
-};
+}
 
 export const getBRCSongs = async (req, res) => {
   try {
@@ -88,10 +77,10 @@ export const getBRCSongs = async (req, res) => {
       return res.send(brcSongs);
     }
     res.status(404).send();
-  } catch (err) {
+  } catch(err) {
     res.status(500).send(`Could not fetch BRC Songs \n${err}`);
   }
-};
+}
 
 export const getBRCSongById = async (req, res) => {
   try {
@@ -99,29 +88,20 @@ export const getBRCSongById = async (req, res) => {
     if (brcSong) {
       return res.send(brcSong);
     }
-    res
-      .status(404)
-      .send(`BRC Song Resource could not be found at requested location`);
-  } catch (err) {
-    res
-      .status(500)
-      .send(`Could not fetch BRC SONG with ID: ${req.params.id} \n${err}`);
+    res.status(404).send(`BRC Song Resource could not be found at requested location`);
+  } catch(err) {
+    res.status(500).send(`Could not fetch BRC SONG with ID: ${req.params.id} \n${err}`);
   }
-};
+}
 
 export const fetchJSRSongs = async (req) => {
   return await performJSRAction(Actions.fetchWithQuery, Song, null, req?.query);
-};
-
+}
+  
 export const fetchJSRFSongs = async (req) => {
-  return await performJSRFAction(
-    Actions.fetchWithQuery,
-    Song,
-    null,
-    req?.query
-  );
-};
+  return await performJSRFAction(Actions.fetchWithQuery, Song, null, req?.query);
+}
 
 export const fetchBRCSongs = async (req) => {
   return await performBRCAction(Actions.fetchWithQuery, Song, null, req?.query);
-};
+}
