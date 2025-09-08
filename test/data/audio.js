65843254e268efbd4b0a1db97ec22adadd18ca71
Kaipo Wilmeth <kwilmeth@fanthreesixty.com>
2025-09-08 01:04:50 -0500

    add tests

---
+++ test/data/audio.js
@@ -0,0 +1,43 @@
+export const createJsrAudio = (testId) => {
+  return {
+    _id: testId,
+    key: "cube-643c71c08cabe0dcede86917-https://static.wikia.nocookie.net/jetsetradio/images/1/1b/Cube_en_14.wav/revision/latest?cb=20240617181433",
+    ordinality: 0,
+    audioUrl:
+      "https://static.wikia.nocookie.net/jetsetradio/images/1/1b/Cube_en_14.wav/revision/latest?cb=20240617181433",
+    transcription: '"Hey!"',
+    usage: "Greet",
+    category: "Unknown",
+    language: "en",
+    speaker: "Cube",
+    gameId: "64285b7918c8a0231136dc5a",
+    characterId: "643c71c08cabe0dcede86917",
+    source: "Jet Set Radio",
+  };
+};
+
+export const createJsrfAudio = (testId) => {
+  return {
+    _id: testId,
+    key: "a.ku.mu-643c717a8cabe0dcede86887-https://static.wikia.nocookie.net/jetsetradio/images/d/d9/Pv_akumu_4.wav/revision/latest?cb=20240618133248",
+    ordinality: 5,
+    audioUrl:
+      "https://static.wikia.nocookie.net/jetsetradio/images/d/d9/Pv_akumu_4.wav/revision/latest?cb=20240618133248",
+    metadata: {
+      duration: 0.6662131519274377,
+      codec: "PCM",
+      mimeType: "audio/wave",
+      sampleRate: 22050,
+      bitrate: 352800,
+      numberOfChannels: 1,
+    },
+    transcription: '"Ha!"',
+    usage: "Trick",
+    category: "Playable",
+    language: "en",
+    speaker: "A.KU.MU",
+    gameId: "64285b7918c8a0231136dc5d",
+    characterId: "643c717a8cabe0dcede86887",
+    source: "Jet Set Radio Future",
+  };
+};
